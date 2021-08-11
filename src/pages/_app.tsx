c4341616836495257407ef0cfd38a23813d27974
harutete <ai.takeuchi.10@gmail.com>
2021-08-11 21:44:03 +0900

    husky追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
-const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
+import { AppProps } from 'next/app'
 
-export default MyApp
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
